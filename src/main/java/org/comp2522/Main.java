765d54a8345c903fe70b2aabf14e4a43289cbe9b
kemp-liao <kempliao@gmail.com>
2022-09-06 17:13:15 -0700

    Fixed checkstyle errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+ * Small program that print a message on screen.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Dog Dog Dog");
-    }
+  public static void main(String[] args) {
+    System.out.println("Dog Dog Dog");
+  }
 }
